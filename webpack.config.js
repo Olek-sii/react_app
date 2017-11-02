58d58764a11dc2d425bb0f7bb9de8a9478ca7a54
Danil <danilv@123software.ru>
2017-11-02 18:22:12 +0200

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -5,26 +5,26 @@ var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-  entry: APP_DIR + '/index.jsx',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-        //{ test: /\.css$/, use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})) },
-        //{ test: /\.scss$/, use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', 'sass-loader']})) },
-        { test: /\.jsx?$/, loader: 'babel-loader', exclude: [/node_modules/, /public/] },
-        { test: /\.jsx?$/, loader: 'eslint-loader', include: APP_DIR, enforce: 'pre' }
-    ],
-    loaders: [
-      {
-          test: /\.jsx?/,
-          include: APP_DIR,
-          loader: 'babel-loader'
-      }
-    ]
-  }
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            // { test: /\.css$/, use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})) },
+            // { test: /\.scss$/, use: ['css-hot-loader'].concat(ExtractTextPlugin.extract({fallback: 'style-loader', use: ['css-loader', 'sass-loader']})) },
+            { test: /\.jsx?$/, loader: 'babel-loader', exclude: [/node_modules/, /public/] },
+            { test: /\.jsx?$/, loader: 'eslint-loader', include: APP_DIR, enforce: 'pre' }
+        ],
+        loaders: [
+            {
+                test: /\.jsx?/,
+                include: APP_DIR,
+                loader: 'babel-loader'
+            }
+        ]
+    }
 };
 
 module.exports = config;
